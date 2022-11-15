bf9250a76a7bfb7a425a89367ca65c7653de566c
kkrajpurohit007 <kkrajpurohit007@gmail.com>
2022-11-15 05:35:06 +0530

    config server

---
+++ auction-config-server/src/main/java/com/auction/app/AuctionConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.auction.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionConfigServerApplication.class, args);
+    }
+
+}
