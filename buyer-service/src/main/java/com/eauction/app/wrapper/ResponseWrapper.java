af1d708a2b5c6a07a8cadfc4f0ada013c13ddca3
kkrajpurohit007 <kkrajpurohit007@gmail.com>
2022-11-17 06:32:36 +0530

    config server initat

--- buyer-service/src/main/java/com/eauction/app/wrapper/ResponseWrapper.java
+++ buyer-service/src/main/java/com/eauction/app/wrapper/ResponseWrapper.java
@@ -1,4 +1,14 @@
 package com.eauction.app.wrapper;
 
+import com.eauction.app.model.Buyer;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
 public class ResponseWrapper {
+    private Buyer buyer;
+    private ProductBid productBid;
+    private List<ProductBid> productBids;
 }
--- seller-service/src/main/java/com/eauction/app/SellerServiceApplication.java
+++ seller-service/src/main/java/com/eauction/app/SellerServiceApplication.java
@@ -2,6 +2,7 @@ package com.eauction.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableEurekaClient
 @OpenAPIDefinition(info = @Info(title = "Seller API", version = "2.0", description = "Seller Information"))
 public class SellerServiceApplication {
 
